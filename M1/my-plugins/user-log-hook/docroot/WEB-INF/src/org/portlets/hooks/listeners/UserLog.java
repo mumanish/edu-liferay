2d72774d623e56109158052fddb8640dba275cc2
Vladimir Frolov <frolovhub@gmail.com>
2012-11-19 21:54:21 +0400

    LFA[mine-ULH]: several fields logging

--- M1/my-plugins/user-log-hook/docroot/WEB-INF/src/org/portlets/hooks/listeners/UserLog.java
+++ M1/my-plugins/user-log-hook/docroot/WEB-INF/src/org/portlets/hooks/listeners/UserLog.java
@@ -11,39 +11,56 @@ import java.util.*;
 
 public class UserLog extends BaseModelListener<User> {
 
-	String logs;
-	String mnameA, mnameB;
+	String logs, oldValue;
+	String[] attributes, tempValue = new String[20];
+	
 
-	public void onAfterCreate(User user) throws ModelListenerException {
-		System.out.println("New user created : " + user.getFullName());
+	public void setTempValue(String t, int i) {
+		this.tempValue[i] = t;
+	}
+
+	public String getTempValue(int i) {
+		return tempValue[i];
 	}
 
 	public void onBeforeUpdate(User user) throws ModelListenerException {
+		
+		attributes = new String[20];
 
-		mnameA = user.getMiddleName();
+		attributes[0] = user.getJobTitle();
+		attributes[1] = user.getMiddleName();
+		attributes[2] = user.getFirstName();
+		
+		doLog(attributes[0], 0, user);	
+		doLog(attributes[1], 1, user);	
+		doLog(attributes[2], 2, user);
 
-		mnameB = user.getJobTitle();
 
-		user.setJobTitle(mnameA);	//AFTER CHANGE
-		user.setLastName(mnameB);	//BEFORE CHANGE
+	}
+
+
+	public void onAfterUpdate(User user) throws ModelListenerException {
+		System.out.println("Updated user: " + user.getFullName());
+		System.out.println("\n===| LOG |===\n" + user.getComments());
+	}
+
+
+
+	public void doLog(String attr, int i, User user) {
+
+		oldValue = getTempValue(i);		
+		setTempValue(attr, i);
 
 		GregorianCalendar calendar = new GregorianCalendar();
 		logs = user.getComments();
 
-
-		if (mnameB != mnameA) {
-			System.out.println("MNAME CHANGED!" + mnameB + "=" + mnameA);
+		if (attr.equals(oldValue)) {
+			System.out.println("\n\nVALUE NOT CHANGED: " + oldValue + "=" + attr);
+			
 		} else {
-			System.out.println("MNAME NOT CHANGED" + mnameB + "=" + mnameA);
-
-		}
-		user.setComments(logs + "\nUpdated on: " + calendar.getTime() + "\n" + "Attribute changed. Middle Name: from " + mnameB + " to " + mnameA);
-
-	}
-
-	public void onAfterUpdate(User user) throws ModelListenerException {
-		System.out.println("Updated user: " + user.getFullName());
-		System.out.println(user.getComments());
-	}
+			System.out.println("\n\nVALUE CHANGED: " + oldValue + "=" + attr);
+			user.setComments(logs + "\n\nUpdated on: " + calendar.getTime() + "\n" + "Attribute changed: from " + oldValue + " to " + attr);
+		}		
+	}	
 
 }
